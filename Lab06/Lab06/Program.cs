eee1cf9b84bfefdaaed9dcf5684591f9e5fe97e4
Collin <collinthenewmaker@gmail.com>
2025-11-15 20:03:30 +0400

    Done shit

--- Lab06/Lab06/Program.cs
+++ Lab06/Lab06/Program.cs
@@ -4,7 +4,39 @@
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Лабораторная работа 03. Выполнил студент 6104-020302D Круглов Данил");
+            //string inputString = "Съешь ещё этих мягких французских булочек, да выпей же чаю.";
+            string inputString = "Съешь ещё эих мягких французских булочек, да выпей же магаданского чаю";
+            Console.WriteLine("Исходная строка: " + inputString + "\n");
+            Console.WriteLine(StringTools.ReplaceWord(inputString, "гриша", "Пидор"));
+            Console.Write("Кол-во букв: ");
+            Console.WriteLine(StringTools.LetterCount(inputString));
+            Console.Write("Кол-во прочих символов: ");
+            Console.WriteLine(StringTools.NonLetterCount(inputString));
+            Console.Write("Длина строки в целом: ");
+            Console.WriteLine(StringTools.SymbolCount(inputString));
+            Console.Write("Средняя длина слова: ");
+            Console.WriteLine(StringTools.AverageWordLength(inputString));
+            Console.Write("Заменить слово \"{0}\" на \"{1}\": ", "да", "нет");
+            Console.WriteLine(StringTools.ReplaceWord(inputString, "да", "нет"));
+            Console.Write("Вхождений подстроки \"{0}\": ", "да");
+            Console.WriteLine(StringTools.SubstringCount(inputString, "да"));
+            if (StringTools.IsAPalindrome(inputString))
+            {
+                Console.WriteLine("Строка является палиндромом.");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом.");
+            }
+            if (StringTools.IsAValidDate(inputString))
+            {
+                Console.WriteLine("Строка является датой.");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является датой.");
+            }
         }
     }
 }
--- Lab06/Lab06/StringTools.cs
+++ Lab06/Lab06/StringTools.cs
@@ -1,7 +1,188 @@
+using System.Text;
+
 namespace Lab06
 {
-    public class StringTools
-    {
+	public class StringTools
+	{
+		// Содержит большие и маленькие буквы латиницы и кириллицы
+		private const string _letters = "йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 
-    }
+		private const string _numbers = "1234567890";
+
+		// Внутренний метод реализации LetterCount и SymbolCount. str - строка, где ищем символы. keys - символы, которые мы ищем. inverted - наоборот, искать символы, НЕ содержащиеся в keys
+		private static int _UniversalCount(string str, string keys, bool inverted = false)
+		{
+			int result = 0;
+			for (int index = 0; index < str.Length; index++)
+			{
+				char letter = str[index];
+				// Если в строке нет искомой подстроки, IndexOf() вернёт число -1
+				if (!inverted)
+				{
+					if (_IsInKeys(letter, keys)) result++;
+				}
+				else
+				{
+					if (!_IsInKeys(letter, keys)) result++;
+				}
+			}
+			return result;
+		}
+
+		private static bool _IsInKeys(char chr, string keys)
+		{
+			return keys.IndexOf(chr) != -1; ;
+		}
+
+		private static bool _IsALetter(char chr)
+		{
+			return _IsInKeys(chr, _letters);
+		}
+
+		public static int LetterCount(string str)
+		{
+			return _UniversalCount(str, _letters);
+		}
+
+		public static int NonLetterCount(string str)
+		{
+			return _UniversalCount(str, _letters, true);
+		}
+
+		public static int SymbolCount(string str)
+		{
+			return str.Length;
+		}
+
+		private static List<int> _GetWordsData(string str, string keys = _letters) // Понятие "слова" может быть заменено, если в keys передать другой набор символов, например цифр
+		{
+			List<int> result = new List<int>(); /* Мы ещё не знаем, сколько слов будет в итоге, поэтому используем List (коллекцию)
+			В result будут храниться индексы начал слов и длины слов.
+			[0] - начало первого слова
+			[1] - длина первого слова
+			[2] - начало второго слова
+			...
+			*/
+			int index = 0;
+			while (index < str.Length)
+			{
+				while (index < str.Length && !_IsInKeys(str[index], keys)) index++; // Ищем следующую букву
+				if (index < str.Length) // Продолжаем только если мы в пределах строки
+				{
+					int wordStartIndex = index; // Первая буква в слове
+					index++;
+					while (index < str.Length && _IsInKeys(str[index], keys)) index++; /* Ищем следующую небукву
+					Сейчас wordStartIndex - первая буква слова, а index - символ "после" последней буквы слова.
+					*/
+					result.Add(wordStartIndex);
+					result.Add(index - wordStartIndex);
+				}
+			}
+			return result;
+		}
+
+		private static string[] _GetWords(string str, string keys = _letters)
+		{
+			List<int> wordsData = _GetWordsData(str, keys);
+			string[] result = new string[wordsData.Count / 2]; // Тут нам уже известно, сколько будет в итоге слов. поэтому можно использовать массив
+			for (int index = 0; index < wordsData.Count; index += 2)
+			{
+				result[index / 2] = (str.Substring(wordsData[index], wordsData[index + 1]));
+			}
+			return result;
+		}
+
+		public static double AverageWordLength(string str)
+		{
+			string[] words = _GetWords(str);
+			int LetterCount = 0;
+			for (int index = 0; index < words.Length; index++)
+			{
+				LetterCount += words[index].Length;
+			}
+			return (double)LetterCount / (double)words.Length;
+		}
+
+		public static string ReplaceWord(string str, string wordToSeek, string wordToInsert)
+		{
+			/* Чтобы эффективно изменять строку (неизменяемый тип данных)
+			конвертируём её в StringBuilder, который позволяет изменять свой состав динамически
+			*/
+			StringBuilder sb = new StringBuilder(str);
+			List<int> wordsData = _GetWordsData(str);
+			// Чтобы индексы не поломались, будем работать с конца:
+			for (int index = wordsData.Count - 2; index >= 0; index -= 2)
+			{
+				int wordStartIndex = wordsData[index];
+				int wordLength = wordsData[index + 1];
+				string word = str.Substring(wordStartIndex, wordLength);
+				if (word.ToLower() == wordToSeek.ToLower()) // Так как регистр нужно игнорировать, ставим ToLower()
+				{
+					sb.Remove(wordStartIndex, wordLength); // Удаляем слово
+					sb.Insert(wordStartIndex, wordToInsert); // Вставляем новое слово
+				}
+			}
+			return sb.ToString(); // "Собираем" строку заново
+		}
+
+		public static int SubstringCount(string str, string substr)
+		{
+			int index = 0;
+			int count = 0;
+			do
+			{
+				index = str.IndexOf(substr, index + 1);
+				if (index != -1) count++;
+			} while (index != -1);
+			return count;
+		}
+
+		public static bool IsAPalindrome(string str)
+		{
+			// Проверку на то, является ли строка словом ИЗ букв, не делаем. Число тоже может быть палиндромом (20.11.02 , 22:22 итд)
+			for (int index = 0; index < str.Length / 2; index++) // Проверяем до середины слова (или не включая до символа, находящегося по середине)
+			{
+				if (str[index] != str[str.Length - index - 1]) return false; // Если символ не равен символу с другой стороны строки, сразу можно сказать что не палиндром и вернуть false
+			}
+			return true;
+		}
+
+		public static bool IsAValidDate(string str)
+		{
+			/* Получаем данные о нахождении "слов", при этом под "словом" будем считать
+			число, то есть подстроку состоящую из цифр */
+			List<int> wordsData = _GetWordsData(str, _numbers);
+			// XX.XX.XX    XX.XX.XXXX
+			// 01234567    0123456789
+			List<int> dateFormat1 = new List<int>() { 0, 2, 3, 2, 6, 2 };
+			List<int> dateFormat2 = new List<int>() { 0, 2, 3, 2, 6, 4 };
+			/*
+			Если число является датой, то информация о словах ВСЕГДА будет
+			соответствовать формату 1 (если в доте неполный год)
+			или формату 2 (полный год)
+			*/
+			if (wordsData == dateFormat1 || wordsData == dateFormat2)
+			{
+				// Формат соответствует. Теперь нужно проверить, правильные ли числа и месяцы
+				string[] words = _GetWords(str, _numbers);
+				int day = Convert.ToInt32(words[0]);
+				int month = Convert.ToInt32(words[1]);
+				if (month > 12 || month == 0 || day == 0)
+				{
+					return false;
+				}
+				else
+				{
+					if (month % 2 == 0)
+					{
+						// Четный месяц: февраль, апрель, 
+					}
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
 }
